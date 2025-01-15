459b3371d6c745aa54d52b15e80dba1d8cb19eec
DominicNikolai <d960960gm@gmail.com>
2025-01-15 10:06:00 +0100

    v0.0.6

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const $hero = $('.hero');
 const TOTAL_FRAMES = 74;
 
 function generateSrc(nFrame) {
-  return `./assets/frames/moto-${nFrame}.webp`;
+  return `./assets/frames/taco-${nFrame}.webp`;
 }
 
 function updateImage(nFrame) {
